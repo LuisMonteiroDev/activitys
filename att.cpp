a2c8b9ebaf11c7685b8e234a3482ae4a826f06d7
Luis Monteiro <felipemoreno9065@gmail.com>
2023-09-09 22:41:41 -0300

    Aticvity 1: area of place

---
+++ att.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float c, l, a, p;
+    int qe, qaf, qf;
+    qe = 0;
+    qf = 8;
+    qaf = 0;
+    cin>>c;
+    cin>>l;
+    a=c*l;
+    
+    cout<<"Areá = "<< a;
+
+    p = c+l+c+l;    
+    qe = (p/2)+1
+    qaf = qf*p;
+
+    return 0;
+}
